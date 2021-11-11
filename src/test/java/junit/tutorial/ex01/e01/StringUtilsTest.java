2249f9e0ec2e5e25e25c03d1319dbf2cc55399dc
tabatayuuki <yuki.tabata@rakus-partners.co.jp>
2021-11-11 12:12:26 +0900

    1-1end

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -27,8 +27,25 @@ class StringUtilsTest {
 	}
 
 	@Test
-	void testToSnakeCase() {
-		fail("Not yet implemented");
+	void test1() {
+		String result = StringUtils.toSnakeCase("aaa");
+		
+		assertEquals("aaa", result, "aaa._error");
 	}
+	@Test
+	void test2() {
+		String result = StringUtils.toSnakeCase("HelloWorld");
+		
+		assertEquals("hello_world", result, "hello_world_error");
+	}
+	@Test
+	void test3() {
+		String result = StringUtils.toSnakeCase("practiceJunit");
+		
+		assertEquals("practice_junit", result, "practice_junit_error");
+	}
+		
+		
+	
 
 }
